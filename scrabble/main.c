6bcc839ee7a3b5eb11cbbc07cc0cae0a1d804198
KristianKirchev <kris.kirchev@gmail.com>
2021-06-24 15:52:48 +0300

    add valid again

--- scrabble/main.c
+++ scrabble/main.c
@@ -119,6 +119,60 @@ void random_word(struct trie_node_t *root, int letters, char *word){
     return;
 }
 
+int search(struct trie_node_t *root, char *word)
+{
+    if(root == NULL) {
+        return 0;
+    }
+
+    struct trie_node_t* curr = root;
+
+    for(int i = 0; word[i] != 0; i++){
+
+        curr = curr->children[word[i] - 'a'];
+        if(curr == NULL){
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int validation(struct trie_node_t *root, char *answer, char *letters){
+    int flag;
+
+    if(strlen(answer)>strlen(letters)){
+
+        return 0;
+    }
+
+    for(int i = 0; answer[i] != 0; i++){
+
+        for(int j = 0; letters[j] != 0; j++){
+
+            if(answer[i] == letters[j]){
+
+                flag = 0;
+                letters[j] ='!';
+                goto located;
+            }
+            else{
+                flag = 1;
+            }
+        }
+        located:
+        if(flag == 1){
+            return 0;
+        }
+    }
+
+    if(search(root, answer) == 0){
+        return 0;
+    }
+
+    return 1;
+}
+
 int start(struct trie_node_t* root, int turns, int letters) {
 
   char rand_letters[letters];
