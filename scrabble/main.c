84441c1cd9df29fbd8afe4acbc813e01fe848f38
KristianKirchev <kris.kirchev@gmail.com>
2021-06-26 17:15:17 +0300

    update

--- scrabble/main.c
+++ scrabble/main.c
@@ -451,6 +451,34 @@ void delete_trie_file(){
   fclose(fp);
 }
 
+void read_trie(char *fill){
+
+  FILE *file = fopen("../trie_file.txt", "r");
+  char word[INPUT_WORD_SIZE];
+
+  int word_count = 0;
+
+  fill[word_count] = malloc(INPUT_WORD_SIZE);
+
+  while (fgets(fill[word_count], INPUT_WORD_SIZE, file) != NULL)
+  {
+      fputs(fill[word_count], stdout);
+      //fputs()
+      word_count++;
+      fill[word_count] = malloc(INPUT_WORD_SIZE);
+  }
+
+  for (int i = 0; i < NUMBER_OF_WORDS; i++)
+  {
+      if(debug(fill[i]) != 1){
+          break;
+      }
+      printf("\n%s\n", fill[i]);
+
+  }
+
+}
+
 int main() {
 
   int command;
@@ -497,6 +525,8 @@ int main() {
     char print_word[INPUT_WORD_SIZE];
     int level = 0;
     display_Trie_in_File(root, trie_words, level, print_word);
+    char *read_trie_from_file[INPUT_WORD_SIZE];
+    //read_trie(read_trie_from_file);
 
 
 start:
